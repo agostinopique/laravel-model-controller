ee194e39ea6f9fa8f45a8f1aee95b57444fd3f11
agostinopique <ago.pique@yahoo.it>
2022-06-26 17:05:24 +0200

    Stampati dinamicamente tutti i film del database in layout grezzo;

--- resources/views/movies.blade.php
+++ resources/views/movies.blade.php
@@ -4,15 +4,15 @@
 
 <h1>Our Movies</h1>
 
-@dump($movies);
+{{-- @dump($movies) --}}
 
 @foreach($movies as $movie)
 <ul>
     <li>{{$movie->title}}</li>
     <li>{{$movie->original_title}}</li>
-    <li>{{$movie->title}}</li>
-    <li>{{$movie->title}}</li>
-    <li>{{$movie->title}}</li>
+    <li>{{$movie->nationality}}</li>
+    <li>{{$movie->date}}</li>
+    <li>{{$movie->vote}}</li>
 </ul>
 @endforeach
 
